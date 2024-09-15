d921674f0d0296d18833987e0ad05f455179bb7b
Nyakamba <omwegaenock@gmail.com>
2024-09-15 22:27:47 +0300

    creating cutom mutation hook

---
+++ src/react-query/hooks/useAddTodo.ts
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "./useTodos";
+import axios from "axios";
+import { CACHE_KEY_TODOS } from "../constants";
+
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
+const useAddTodo = (onAdd: () => void) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", { todo })
+        .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previousTodos =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
+        newTodo,
+        ...todos,
+      ]);
+
+      onAdd();
+
+      return { previousTodos };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      //APROACH 1: upadting data in the server
+      //queryClient.invalidateQueries({ queryKey: CACHE_KEY_TODOS });
+      //APPROACH 2:Updtating data in the catch
+
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
+    },
+  });
+};
+
+export default useAddTodo;
